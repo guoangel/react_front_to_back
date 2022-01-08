0e4ed9a5c731a07cfc2e91b880ec2a94097457b7
angel <fsguoangel@hotmail.com>
2022-01-08 15:19:16 +0800

    Forms, validation and Animation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,52 @@
-
-import './App.css';
-import FeedbackItem from './components/FeedbackItem';
-import Header from './components/Header';
+import { v4 as uuidv4} from 'uuid'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import { useState } from "react"
+import Header from "./components/Header"
+import FeedbackList from "./components/FeedbackList"
+import FeedbackStats from "./components/FeedbackStats"
+import FeedbackForm from "./components/FeedbackForm"
+import FeedbackData from "./data/FeedbackData"
+import AboutIconLink from './components/AboutIconLink'
+import AboutPage from "./pages/AboutPage"
 
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData)
+
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4()
+    setFeedback([newFeedback, ...feedback])
+  }
+
+  const deleteFeedback = (id) => {
+    if(window.confirm('Are you sure you want to delete?')) {
+      // filter the id being passed in and return new array
+      setFeedback(feedback.filter((item) => item.id !== id))
+    }
+  }
+
   return (
-    <>
+    <Router>
       <Header />
-      <div className="container">
-        <FeedbackItem />
+      <div className='container'>
+        <Routes>
+          <Route exact path='/' element={
+            <>
+              <FeedbackForm handleAdd={addFeedback}/>
+              <FeedbackStats feedback={feedback} />
+              <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/> 
+            </>
+          }>
+          </Route>
+
+          <Route path='/about' element={<AboutPage />}></Route> 
+
+        </Routes>
+
+        <AboutIconLink />
+        
       </div>
-    </>
-  );
+    </Router>
+  )
 }
 
 export default App;
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,23 +1,23 @@
-import PropTypes from "prop-types"
-import {useState} from 'react'
+import { FaTimes } from 'react-icons/fa'   // import the FaTimes cion from the font awesome library
+import PropTypes from 'prop-types'
+import Card from './shared/Card'
 
-function FeedbackItem({ item }) {
-    const [rating, setRating] = useState(7)
-    const [text, setText] = useState('this is an example of a feedback item')
+function FeedbackItem({ item, handleDelete }) {
 
-    const handleClick = () => { 
-        setRating((prev) => {
-            return prev +1
-        })
-     }
-    return (
-      <div className="card">
-        <div className="num-display">{rating}</div>
-        <div className="text-display"> {text} </div>
-        <button onClick={handleClick}>Click Me</button>
-      </div>
-    );
-  }
+  return (
+    <Card>
+      <div className="num-display">{item.rating}</div>
+      <button onClick={() => handleDelete(item.id)} className="close">
+        {/* FaTimes takes in a color */}
+        <FaTimes color='purple' />
+      </button>
+      <div className="text-display">{item.text}</div>
+    </Card>
+  )
+}
 
+FeedbackItem.propTypes = {
+  item: PropTypes.object.isRequired,
+}
 
 export default FeedbackItem
